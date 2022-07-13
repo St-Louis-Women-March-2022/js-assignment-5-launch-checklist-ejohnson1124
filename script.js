6b961a91da4ce03d4cb667dec777674149c2bdac
Erica <you@example.com>
2022-07-12 20:56:24 -0500

    more edit

--- script.js
+++ script.js
@@ -3,10 +3,26 @@
 const { myFetch } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let pilotName = document.querySelector("input[name=pilotName]");
+        let pilotValue = pilotName.value;
+        let copilotName = document.querySelector("input[name=copilotName]");
+        let copilotValue = copilotName.value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let fuelLevelValue = fuelLevel.value;
+        let cargoMass = document.querySelector("input[name=cargoMass]");
+        let cargoMassValue = cargoMass.value;
+        
+        formSubmission(document,pilotValue,copilotValue,fuelLevelValue,cargoMassValue);
+        
 
+    });
+    
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse = myFetch;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
